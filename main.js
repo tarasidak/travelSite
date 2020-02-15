3dd0a7d0cb139cbe09604d41a26f923347ce1b94
Taras Sidak <tarasidak1990@gmail.com>
2020-02-15 16:33:39 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+/* MENU ACCOUNT*/
 let btnLi = document.getElementById('dropDownMenu');
 let menu = document.getElementById('dropMenu');
 let body = document.querySelector('body');
@@ -14,3 +16,29 @@ body.onclick = function(event){
         console.log("none")
     }
 };
+
+/*MOBILE SLIDER*/
+
+let slideIndex = 1;
+let slides = document.getElementsByClassName("mySlides");
+  showSlides(slides);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+
